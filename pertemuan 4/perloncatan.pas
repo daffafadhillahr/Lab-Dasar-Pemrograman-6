badc705b83bcba5f79a3fdf73c73868371c17c36
daffafadhillahr <daffafadhillahr2104@gmail.com>
2025-10-23 23:13:04 +0700

    pertemuan 4 : perulangan

---
+++ pertemuan 4/perloncatan.pas
@@ -0,0 +1,16 @@
+uses crt;
+label a, b, c, d, e;
+
+begin
+clrscr; 
+a:writeln('hello world'); 
+goto b;
+d:writeln('fasilkom-ti');
+goto e;
+c:writeln('ilmu komputer'); 
+goto d;
+b:writeln('IKLC');
+goto c; 
+e:
+
+end.
